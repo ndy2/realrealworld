bb2261fc63bf3ba564bb8e4d2d329def0cc69c8a
ndy2 <emrdbs12@gmail.com>
2022-11-01 20:35:50 +0900

    chore: fix @JsonRootname

--- src/main/kotlin/com/example/realworld/domain/article/model/inout/AddComment.kt
+++ src/main/kotlin/com/example/realworld/domain/article/model/inout/AddComment.kt
@@ -1,9 +1,11 @@
 package com.example.realworld.domain.article.model.inout
 
 import com.example.realworld.exception.SelfValidating
+import com.fasterxml.jackson.annotation.JsonRootName
 import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.NotNull
 
+@JsonRootName("comment")
 data class AddComment(
 
     @field:NotNull(message = "can't be missing")
